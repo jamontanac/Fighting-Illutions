92e0a3e8a5f7518b6ada8ce585b7f74ab31a93f8
Jose Alejandro <jamontanac@unal.edu.co>
2023-10-21 01:58:45 -0500

    Up to this point the defenses ara integrated to the pipeline and there is not a simple way to integrate new defenses other than add it to the pipeline and the respective report

--- Fighting-Illutions/src/Fighting-Illutions/pipelines/defenses/nodes.py
+++ Fighting-Illutions/src/Fighting-Illutions/pipelines/defenses/nodes.py
@@ -12,10 +12,33 @@ import torch
 import torchvision
 import torchvision.transforms as transforms
 import cv2
+from torchvision.transforms import Compose
+from typing import Tuple, Dict
 
-
-
+import torch.nn as nn
+from torch.utils.data import Dataset
+from torch.utils.data import DataLoader
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+#We define the dataset to load the adversarial results
+class AdversarialDataset(Dataset):
+    def __init__(self, data_dict, transform=None):
+        """
+        Args:
+            data_dict (dict): Dictionary containing adversarial data.
+            transform (callable, optional): Optional transform to be applied on the examples.
+        """
+        self.data_dict = data_dict
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.data_dict["real_labels"])
+
+    def __getitem__(self, idx):
+        sample = {key: value[idx] for key, value in self.data_dict.items()}
+        if self.transform:
+            sample["examples"] = self.transform(sample["examples"])
+        return sample
 
 def Create_data_loader(batch_size=128)-> torch.utils.data.DataLoader:
 
@@ -29,6 +52,7 @@ def Create_data_loader(batch_size=128)-> torch.utils.data.DataLoader:
     testloader = torch.utils.data.DataLoader(
         testset, batch_size=batch_size, shuffle=False, num_workers=2)
     return testloader
+
 
 def resize_pad(image, ratio=0.8):
     """
@@ -74,7 +98,7 @@ def random_distortion(h, w, d, delta):
     return distorted_coords
 
 
-def image_distortion(img, d, delta):
+def image_distortion(img, d=4, delta=0.5):
     """
     Apply distortion to a given image.
     img: a tensor of shape (C, H, W)
@@ -99,3 +123,89 @@ def image_distortion(img, d, delta):
                         distorted_image[:, dest_y + j, dest_x + i] = img[:, src_y + j, src_x + i]
                         
     return distorted_image
+# Here we define the transformations to be used in the dataloader
+class ResizePadTransform:
+    def __init__(self, ratio=0.8):
+        self.ratio = ratio
+
+    def __call__(self, image):
+        return resize_pad(image, ratio=self.ratio)
+class DistortTransform:
+    def __init__(self, d=4, delta=0.5):
+        self.d = d
+        self.delta = delta
+        
+    def __call__(self, img):
+        return image_distortion(img, self.d, self.delta)
+
+
+
+def init_model(model:nn.Module)->torch.nn.Module:
+    # Move model to GPU if available
+    model = model.to(device)
+    if device == 'cuda':
+        model= torch.nn.DataParallel(model)
+        cudnn.benchmark = True
+    return model
+
+def Padding_defense(dataset: Dict, params:Dict) -> torch.utils.data.DataLoader:
+    transform_fn = ResizePadTransform(ratio = params["padding_ratio"])
+    dataset = AdversarialDataset(dataset, transform=transform_fn)
+    dataloader = DataLoader(dataset, batch_size=512, shuffle=True)
+    return dataloader
+
+def Distort_defense(dataset: Dict, params:Dict) -> torch.utils.data.DataLoader:
+    transform_fn = DistortTransform(d = params["window_size"], delta=params["window_size"])
+    dataset = AdversarialDataset(dataset, transform=transform_fn)
+    dataloader = DataLoader(dataset, batch_size=512, shuffle=True)
+    return dataloader
+
+def Padding_Distort_defense(dataset: Dict, params:Dict) -> torch.utils.data.DataLoader:
+    combined_transform = Compose([
+        PaddingTransform(ratio=params["ratio"]),
+        DistortTransform(d=params["window_size"], delta=params["delta"])
+    ])
+    dataset = AdversarialDataset(dataset, transform=combined_transform)
+    dataloader = DataLoader(dataset, batch_size=512, shuffle=True)
+    return dataloader
+def Report(dataloader:torch.utils.data.DataLoader,model:nn.Module):
+    model_classifier = init_model(model)
+    model_classifier.eval()
+    correct_defense = 0
+    correct_model = 0
+    correct_adversarial = 0
+    total = 0
+    confidence_defense = []
+    true_labels = []
+    model_predictions = []
+    adversarial_predictions = []
+    defense_predictions = []
+    for batch in dataloader:
+        images, real_labels = batch["examples"], batch["real_labels"]
+        model_labels, adversarial_labels = batch["model_labels"], batch["adversarial_labels"]
+    
+        images, real_labels = images.to(device), real_labels.to(device)
+        model_labels, adversarial_labels = model_labels.to(device), adversarial_labels.to(device)
+        with torch.no_grad():
+            outputs = model_classifier(images)
+            _, predicted = torch.max(outputs.data, 1)
+            confidence_defense.extend(F.softmax(outputs, dim=1).max(dim=1)[0].cpu().numpy())
+            total += real_labels.size(0)
+            correct_defense += (predicted == real_labels).sum().item()
+            correct_model += (model_labels == real_labels).sum().item()
+            correct_adversarial += (adversarial_labels == real_labels).sum().item()
+            true_labels.extend(real_labels.cpu().numpy())
+            model_predictions.extend(model_labels.cpu().numpy())
+            adversarial_predictions.extend(adversarial_labels.cpu().numpy())
+            defense_predictions.extend(predicted.cpu().numpy())
+
+    original_accuracy = correct_model *100 /total
+    adversarial_accuracy = correct_adversarial *100 /total
+    defense_accuracy = correct_defense *100 /total
+    return {"accuracy":original_accuracy, "Adversarial_accuracy":adversarial_accuracy,"Defense_accuracy":defense_accuracy}
+
+# Combined transform
+# combined_transform = Compose([
+#     PaddingTransform(ratio=0.9),
+#     DistortTransform(d=4, delta=0.5)
+# ])
--- Fighting-Illutions/src/Fighting-Illutions/pipelines/defenses/pipeline.py
+++ Fighting-Illutions/src/Fighting-Illutions/pipelines/defenses/pipeline.py
@@ -6,7 +6,7 @@ generated using Kedro 0.18.13
 # from kedro.pipeline import Pipeline, pipeline
 from kedro.pipeline import Pipeline, node
 from kedro.pipeline.modular_pipeline import pipeline
-from .nodes import resize_pad, image_distortion
+from .nodes import Padding_defense, Distort_defense, Padding_Distort_defense, Report 
 def defense_generation_templete() -> Pipeline:
     """
     This pipeline will run a specific defense over the adersarial
@@ -15,32 +15,68 @@ def defense_generation_templete() -> Pipeline:
     """
     return pipeline(
         [
-            node(func=resize_pad,
-                 inputs=["Adversarial_Data","params:defence_options"],
-                 outputs="Resize_Pad_Image",
-                 name="Resize_Pad_Defense"
+            node(func=Padding_defense,
+                 inputs=["Adversarial_Data","params:defense_options"],
+                 outputs="Padding_Defense_Loader_Image",
+                 name="Padding_Defense"
             ),
-            # node(func=image_distortion,
-            #      inputs=["Adversarial_Data","params:defence_options"],
-            #      outputs="Distortion_Image",
-            #      name="Distortion_Defense"
-            # ),
-        ]
+            node(func=Distort_defense,
+                 inputs=["Adversarial_Data","params:defense_options"],
+                 outputs="Distortion_Defense_Loader_Image",
+                 name="Distortion_Defense"
+            ),
+            node(func=Padding_Distort_defense,
+                 inputs=["Adversarial_Data","params:defense_options"],
+                 outputs="Padding_Distortion_Defense_Loader_Image",
+                 name="Padding_Distortion_Defense"
+                ),
+            node(func=Report,
+                 inputs=["Model","Padding_Defense_Loader_Image"],
+                 outputs="Data_report_Padding",
+                 name="Report_Padding"),
+            node(func=Report,
+                 inputs=["Model","Distortion_Defense_Loader_Image"],
+                 outputs="Data_report_Distortion",
+                 name="Report_Distortion"),
+            node(func=Report,
+                 inputs=["Model","Padding_Distortion_Defense_Loader_Image"],
+                 outputs="Data_report_Padding_Distortion",
+                 name="Report_Padding_Distortion")
+        ],inputs = ["Model","Adversarial_Data"],
+        outputs = ["Data_report_Padding","Data_report_Distortion","Data_report_Padding_Distortion"]
     )
 def create_pipeline(**kwargs) -> Pipeline:
     
     Attacks = ["DeepFool", "CarliniL2", "FSGM", "PGD"]
     Models = ["Resnet_model","Regnet_x_model","Regnet_y_model"]
+    Defenses = ["Padding","Distortion","Padding_Distortion"]
+    
+    #  outputs={"Data_report_Padding":f"{model_ref}_Report_Padding_{attack_type}@Report",
+    #           "Data_report_Distortion":f"{model_ref}_Report_Distortion_{attack_type}@Report",
+    #           "Data_report_Padding_Distortion":f"{model_ref}_Report_Padding_Distortion_{attack_type}@Report"},
+    Defense_pipeline=[]
     for index, model_ref in enumerate(Models):
-        Defense_pipeline = [
-            pipeline(pipe=defense_generation_templete(),
-                     )
-        ]
+        for attack_type in Attacks:
+            output_result = {f"Data_report_{defense}": f"{model_ref}_Report_{defense}_{attack_type}@Report" for defense in Defenses}
+            tmp_pipeline = pipeline(pipe=defense_generation_templete(),
+                     parameters={"params:defense_options":f"params:Parameters_defenses.{model_ref}"},
+                     inputs={"Model":model_ref,"Adversarial_Data":f"{model_ref}_Adversarial_{attack_type}@Dataset"},
+                     outputs=output_result,
+                     namespace=f"{model_ref}_Defenses_{attack_type}")
+            Defense_pipeline.append(tmp_pipeline)
+        # Defense_pipeline = [
+        #     pipeline(pipe=defense_generation_templete(),
+        #              parameters={"params:defense_options":f"params:Parameters_defenses.{model_ref}"},
+        #              inputs={"Model":model_ref,"Adversarial_Data":f"{model_ref}_Adversarial_{attack_type}@Dataset"},
+        #              outputs={f"Data_report_{defense}": f"{model_ref}_Report_{defense}_{attack_type}@Report" for defense in Defenses}
+        #              namespace=f"{model_ref}_Defenses_{attack_type}")
+        #              for attack_type in Attacks
+        # ]
         if index==0:
             final_pipelines = sum(Defense_pipeline)
         else:
             final_pipelines += sum(Defense_pipeline)
 
-    # return final_pipelines
-    return Pipeline([])
+    return final_pipelines
+    # return Pipeline([])
 
